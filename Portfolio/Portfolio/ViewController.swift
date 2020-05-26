99bb5fdf2e3d2a87343a12c07eb72177c6a098d6
Shawn Gee <shawngee@gmail.com>
2020-05-26 08:55:21 -0400

    Created Xcode project

---
+++ Portfolio/Portfolio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Portfolio
+//
+//  Created by Shawn Gee on 5/26/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
